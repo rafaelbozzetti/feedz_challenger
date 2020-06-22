72030ae25e87bce1aed392d24d0d660545325adc
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-06-22 20:51:24 -0300

    task: Include Repository and Data Transfer Object

---
+++ src/Domain/User/Data/UserData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Domain\User\Data;
+
+final class UserData
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /** @var string */
+    public $name;
+
+    /** @var string */
+    public $email;
+
+    /** @var string */
+    public $password;
+}

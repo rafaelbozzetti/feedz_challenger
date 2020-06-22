9fce69d680b07523272c0eab3032f0767710e271
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-06-22 20:29:40 -0300

    task: add route, container and middleware files

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+return function (App $app) {
+
+    $app->get('/', \Feedz\Action\HomeAction::class);
+
+};

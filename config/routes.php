335563750c9ca614aac3f646e92ece8a6d3a7817
Rafael Pereira Bozzetti <rafaelbozzetti@gmail.com>
2020-06-23 13:28:14 -0300

    task: User manager routes

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,28 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
 
-    $app->get('/', \Feedz\Action\HomeAction::class);
+  $app->get('/',
+    \Feedz\Action\HomeAction::class);
 
-    $app->post('/auth', \Feedz\Action\AuthAction::class);
+  $app->post('/auth',
+    \Feedz\Action\AuthAction::class);
+
+    // User
+  $app->group('/users', function (RouteCollectorProxy $group) {
+
+    $group->get('',
+      \Feedz\Action\User\UsersListAction::class);
+
+    $group->get('{:id}',
+      \Feedz\Action\User\UserListAction::class);
+
+    $group->post('',
+      \Feedz\Action\User\UserCreateAction::class);
+
+    });
 
 };

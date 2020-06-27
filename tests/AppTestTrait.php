af7ebb1f23eb4012a29a299858abe0ed5d903125
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-06-26 23:48:52 -0300

    task: phpunit structure, trait class and homeActionTest

---
+++ tests/AppTestTrait.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Feedz\Test;
+
+use DI\Container;
+use InvalidArgumentException;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
+use Slim\App;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use UnexpectedValueException;
+
+/**
+ * Container Trait.
+ */
+trait AppTestTrait
+{
+    /** @var Container */
+    protected $container;
+
+    /** @var App */
+    protected $app;
+
+    /**
+     * Bootstrap app.
+     *
+     * @throws UnexpectedValueException
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->app = require __DIR__ . '/../config/bootstrap.php';
+
+        $container = $this->app->getContainer();
+        if ($container === null) {
+            throw new UnexpectedValueException('Container must be initialized');
+        }
+
+        $this->container = $container;
+    }
+
+    /**
+     * Add mock to container.
+     *
+     * @param string $class The class or interface
+     *
+     * @return MockObject The mock
+     */
+    protected function mock(string $class): MockObject
+    {
+        if (!class_exists($class)) {
+            throw new InvalidArgumentException(sprintf('Class not found: %s', $class));
+        }
+
+        $mock = $this->getMockBuilder($class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->container->set($class, $mock);
+
+        return $mock;
+    }
+
+    /**
+     * Create a server request.
+     *
+     * @param string $method The HTTP method
+     * @param string|UriInterface $uri The URI
+     * @param array $serverParams The server parameters
+     *
+     * @return ServerRequestInterface
+     */
+    protected function createRequest(string $method, $uri, array $serverParams = []): ServerRequestInterface
+    {
+        return (new ServerRequestFactory())->createServerRequest($method, $uri, $serverParams);
+    }
+
+    /**
+     * Create a form request.
+     *
+     * @param string $method The HTTP method
+     * @param string|UriInterface $uri The URI
+     * @param array|null $data The form data
+     *
+     * @return ServerRequestInterface
+     */
+    protected function createFormRequest(string $method, $uri, array $data = null): ServerRequestInterface
+    {
+        $request = $this->createRequest($method, $uri);
+
+        if ($data !== null) {
+            $request = $request->withParsedBody($data);
+        }
+
+        return $request->withHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
+
+}
---
+++ tests/TestCase/Action/HomeActionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Feedz\Test\TestCase\Action\Home;
+
+use Feedz\Test\AppTestTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test.
+ */
+class HomeActionTest extends TestCase
+{
+    use AppTestTrait;
+
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function testAction(): void
+    {
+        $request = $this->createRequest('GET', '/');
+        $response = $this->app->handle($request);
+
+        $this->assertStringContainsString('User Login', (string)$response->getBody());
+        $this->assertSame(200, $response->getStatusCode());
+    }
+
+    /**
+     * Test invalid link.
+     *
+     * @return void
+     */
+    public function testPageNotFound(): void
+    {
+        $request = $this->createRequest('GET', '/not-existing-page');
+        $response = $this->app->handle($request);
+
+        // Assert: Not found
+        $this->assertSame(404, $response->getStatusCode());
+    }
+}
